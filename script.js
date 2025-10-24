bf4365ef98d868943f286d30d4b37d4fca125ccf
WeavileFan <elliotreichstein2@gmail.com>
2025-10-24 15:48:06 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
+  if (localStorageTheme !== null) {
+    return localStorageTheme;
+  }
+
+  if (systemSettingDark.matches) {
+    return "dark";
+  }
+
+  return "light";
+}
+
+const localStorageTheme = localStorage.getItem("theme");
+const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
+
+const button = document.querySelector("[data-theme-toggle]");
+
+button.addEventListener("click", () => {
+  const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+
+  const newCta = newTheme === "dark" ? "Dark Mode" : "Light Mode";
+  button.innerText = newCta;  
+
+  document.querySelector("html").setAttribute("data-theme", newTheme);
+
+  currentThemeSetting = newTheme;
+});
